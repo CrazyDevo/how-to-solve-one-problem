07acd4f2cd2648ab35fb0487ced122e3fe350eb7
AdamB25 <barsialtunit@gmail.com>
2024-01-27 06:31:54 -0500

    M006 done

---
+++ M006/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        String sentence="Get the name of dog:Bob";
+
+        System.out.println(sentence.substring(sentence.indexOf(":")+1,3));
+
+    }
+}
