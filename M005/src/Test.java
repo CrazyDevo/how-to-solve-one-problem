bf25bf2cb748c553ba499489d3bf386c93f48859
AdamB25 <barsialtunit@gmail.com>
2024-01-27 06:09:42 -0500

    M005 done

---
+++ M005/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args) {
+        String result="Total is 45";
+        //I want to increase 5 total
+
+        String newResult=
+                result.substring(0,result.lastIndexOf(" "))+(Integer.parseInt(result.substring(result.lastIndexOf(" ")))+5);
+
+        System.out.println(newResult);
+
+    }
+}
