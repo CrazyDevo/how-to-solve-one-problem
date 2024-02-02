7aaf409089b67624178cd79af9161a48b1167e94
AdamB25 <barsialtunit@gmail.com>
2024-02-02 15:46:58 -0500

    M007 done

--- M005/src/Test.java
+++ M005/src/Test.java
@@ -4,8 +4,7 @@ public class Test {
         String result="Total is 45";
         //I want to increase 5 total
 
-        String newResult=
-                result.substring(0,result.lastIndexOf(" "))+(Integer.parseInt(result.substring(result.lastIndexOf(" ")))+5);
+        String newResult= result.substring(0,result.lastIndexOf(" "))+(Integer.parseInt(result.substring(result.lastIndexOf(" ")))+5);
 
         System.out.println(newResult);
 
---
+++ M007/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int[] numbers={1,2,3,4,5,6,7};
+
+        int[] sorted=new int[numbers.length];
+        //reverse array
+        for (int i = numbers.length; i>=0 ; i--) {
+            sorted[numbers.length-1]=sorted[i];
+        }
+
+    }
+}
