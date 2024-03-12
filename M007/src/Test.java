16d8aaf277a6055bbb9c6e37a23a00c5d3677f66
AdamB25 <barsialtunit@gmail.com>
2024-03-12 18:03:56 -0400

    M007 fixed

--- M007/src/Test.java
+++ M007/src/Test.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) {
         int[] numbers={1,2,3,4,5,6,7};
 
-        int[] sorted=new int[numbers.length];
+        int[] reversed=new int[numbers.length];
         //reverse array
         for (int i = numbers.length; i>=0 ; i--) {
-            sorted[numbers.length-1]=sorted[i];
+            reversed[numbers.length-1]=numbers[i];
         }
 
+        System.out.println("Arrays.toString(sorted) = " + Arrays.toString(reversed));
     }
 }
